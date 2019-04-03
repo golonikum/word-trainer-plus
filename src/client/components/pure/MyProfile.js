5b373f79ed8c017b624bbe16754b4ce7033df395
Nikita Golov <NGolov@veeam.com>
2019-04-03 18:44:20 +0300

    Make MyProfile pure function

--- src/client/components/pure/MyProfile.js
+++ src/client/components/pure/MyProfile.js
@@ -1,16 +1,11 @@
 import React from "react"
 
-const MyProfile = React.createClass({
-	render: function() {
-		return(
-			<div>
-				<h2>My Profile</h2>
-				<p>Hi, {this.props.user.name}!</p>
-				<p>You are {this.props.user.role === 'admin' ? 'an administrator' : 'a simple user'}.</p>
-				<p>You're seeing this page because you logged in successfully! Try logging out, clicking the MyProfile link and then completing the login. You wil notice that it redirects you to MyProfile page :)</p>
-			</div>
-		)
-	}
-})
+const MyProfile = ({ user }) =>
+	<div>
+		<h2>My Profile</h2>
+		<p>Hi, {user.name}!</p>
+		<p>You are {user.role === 'admin' ? 'an administrator' : 'a simple user'}.</p>
+		<p>You're seeing this page because you logged in successfully! Try logging out, clicking the MyProfile link and then completing the login. You wil notice that it redirects you to MyProfile page :)</p>
+	</div>
 
 export default MyProfile
