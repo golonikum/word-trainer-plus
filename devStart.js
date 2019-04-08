df9ce5d05003cf81e03c644c6cdfd7d15f96a837
Nikita Golov <NGolov@veeam.com>
2019-04-08 14:25:37 +0300

    Add ESLint

--- devStart.js
+++
@@ -1,9 +0,0 @@
-/*
-	Called during development only. Allows us to use ES6 and JSX in our server code.
-*/
-
-require('babel-core/register')({
-  presets: ['es2015', 'react']
-});
-
-require("./src/server/index")
